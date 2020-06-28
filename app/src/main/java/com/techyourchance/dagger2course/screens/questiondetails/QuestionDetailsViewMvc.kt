8e14a5c108fb12488f8dfc25398c16aa0bf3348e
Vasiliy Zukanov <techyourchance@gmail.com>
2020-06-28 15:53:49 +0300

    Extraction of UI logic on QuestionDetails screen

---
+++ app/src/main/java/com/techyourchance/dagger2course/screens/questiondetails/QuestionDetailsViewMvc.kt
@@ -0,0 +1,84 @@
+package com.techyourchance.dagger2course.screens.questiondetails
+
+import android.content.Context
+import android.os.Build
+import android.text.Html
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.IdRes
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.techyourchance.dagger2course.R
+import com.techyourchance.dagger2course.questions.Question
+import com.techyourchance.dagger2course.screens.common.toolbar.MyToolbar
+
+class QuestionDetailsViewMvc(
+        private val layoutInflater: LayoutInflater,
+        private val parent: ViewGroup?
+) {
+
+    interface Listener {
+        fun onBackClicked()
+    }
+
+    private val toolbar: MyToolbar
+    private val swipeRefresh: SwipeRefreshLayout
+    private val txtQuestionBody: TextView
+
+    val rootView: View = layoutInflater.inflate(R.layout.layout_question_details, parent, false)
+
+    private val context: Context get() = rootView.context
+
+    private val listeners = HashSet<Listener>()
+
+    init {
+        txtQuestionBody = findViewById(R.id.txt_question_body)
+
+        // init toolbar
+        toolbar = findViewById(R.id.toolbar)
+        toolbar.setNavigateUpListener {
+            for (listener in listeners) {
+                listener.onBackClicked()
+            }
+        }
+
+        // init pull-down-to-refresh (used as a progress indicator)
+        swipeRefresh = findViewById(R.id.swipeRefresh)
+        swipeRefresh.isEnabled = false
+    }
+
+    fun bindQuestionBody(questionBody: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            txtQuestionBody.text = Html.fromHtml(questionBody, Html.FROM_HTML_MODE_LEGACY)
+        } else {
+            @Suppress("DEPRECATION")
+            txtQuestionBody.text = Html.fromHtml(questionBody)
+        }
+    }
+
+    fun showProgressIndication() {
+        swipeRefresh.isRefreshing = true
+    }
+
+    fun hideProgressIndication() {
+        if (swipeRefresh.isRefreshing) {
+            swipeRefresh.isRefreshing = false
+        }
+    }
+
+    private fun <T : View?> findViewById(@IdRes id: Int): T {
+        return rootView.findViewById<T>(id)
+    }
+
+    fun registerListener(listener: Listener) {
+        listeners.add(listener)
+    }
+
+    fun unregisterListener(listener: Listener) {
+        listeners.remove(listener)
+    }
+
+}
