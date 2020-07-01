1691f88d32416c1c7140a609d4127234447d6bf4
Vasiliy Zukanov <techyourchance@gmail.com>
2020-07-01 17:45:58 +0300

    Replacing dependency on Retrofit with dependency on StackoverflowApi

--- app/src/main/java/com/techyourchance/dagger2course/questions/FetchQuestionsUseCase.kt
+++ app/src/main/java/com/techyourchance/dagger2course/questions/FetchQuestionsUseCase.kt
@@ -6,14 +6,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 
-class FetchQuestionsUseCase(private val retrofit: Retrofit) {
+class FetchQuestionsUseCase(private val stackoverflowApi: StackoverflowApi) {
 
     sealed class Result {
         class Success(val questions: List<Question>) : Result()
         object Failure: Result()
     }
-
-    private val stackoverflowApi: StackoverflowApi = retrofit.create(StackoverflowApi::class.java)
 
     suspend fun fetchLatestQuestions(): Result {
         return withContext(Dispatchers.IO) {
