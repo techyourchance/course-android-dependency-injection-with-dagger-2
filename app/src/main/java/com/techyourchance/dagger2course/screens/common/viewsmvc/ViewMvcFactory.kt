fb2b5132c431d8262014b1ee8860db05e5337303
Vasiliy Zukanov <techyourchance@gmail.com>
2020-07-05 16:37:28 +0300

    Refactoring QuestionDetailsViewMvc construction into ViewMvcFactory

--- app/src/main/java/com/techyourchance/dagger2course/screens/common/viewsmvc/ViewMvcFactory.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/common/viewsmvc/ViewMvcFactory.kt
@@ -2,11 +2,16 @@ package com.techyourchance.dagger2course.screens.common.viewsmvc
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.techyourchance.dagger2course.screens.questiondetails.QuestionDetailsViewMvc
 import com.techyourchance.dagger2course.screens.questionslist.QuestionsListViewMvc
 
 class ViewMvcFactory(private val layoutInflater: LayoutInflater) {
 
     fun newQuestionsListViewMvc(parent: ViewGroup?): QuestionsListViewMvc {
         return QuestionsListViewMvc(layoutInflater, parent)
+    }
+
+    fun newQuestionDetailsViewMvc(parent: ViewGroup?): QuestionDetailsViewMvc {
+        return QuestionDetailsViewMvc(layoutInflater, parent)
     }
 }
