f28a5ce1d7aadb913a69586ecb830a326ae4b028
Vasiliy Zukanov <techyourchance@gmail.com>
2020-07-13 16:39:15 +0300

    Refactoring PresentationCompositionRoot into PresentationModule and PresentationComponent pair

---
+++ app/src/main/java/com/techyourchance/dagger2course/common/dependnecyinjection/PresentationComponent.kt
@@ -0,0 +1,23 @@
+package com.techyourchance.dagger2course.common.dependnecyinjection
+
+import com.techyourchance.dagger2course.questions.FetchQuestionDetailsUseCase
+import com.techyourchance.dagger2course.questions.FetchQuestionsUseCase
+import com.techyourchance.dagger2course.screens.common.ScreensNavigator
+import com.techyourchance.dagger2course.screens.common.dialogs.DialogsNavigator
+import com.techyourchance.dagger2course.screens.common.viewsmvc.ViewMvcFactory
+import dagger.Component
+
+@Component(modules = [PresentationModule::class])
+interface PresentationComponent {
+
+    fun viewMvcFactory(): ViewMvcFactory
+
+    fun screensNavigator(): ScreensNavigator
+
+    fun dialogsNavigator(): DialogsNavigator
+
+    fun fetchQuestionsUseCase(): FetchQuestionsUseCase
+
+    fun fetchQuestionDetailsUseCase(): FetchQuestionDetailsUseCase
+
+}
---
+++ app/src/main/java/com/techyourchance/dagger2course/common/dependnecyinjection/PresentationModule.kt
@@ -0,0 +1,43 @@
+package com.techyourchance.dagger2course.common.dependnecyinjection
+
+import android.view.LayoutInflater
+import androidx.fragment.app.FragmentManager
+import com.techyourchance.dagger2course.networking.StackoverflowApi
+import com.techyourchance.dagger2course.questions.FetchQuestionDetailsUseCase
+import com.techyourchance.dagger2course.questions.FetchQuestionsUseCase
+import com.techyourchance.dagger2course.screens.common.dialogs.DialogsNavigator
+import com.techyourchance.dagger2course.screens.common.viewsmvc.ViewMvcFactory
+import dagger.Module
+import dagger.Provides
+
+@Module
+class PresentationModule(private val activityCompositionRoot: ActivityCompositionRoot) {
+
+    @Provides
+    fun layoutInflater() = activityCompositionRoot.layoutInflater
+
+    @Provides
+    fun fragmentManager() = activityCompositionRoot.fragmentManager
+
+    @Provides
+    fun stackoverflowApi() = activityCompositionRoot.stackoverflowApi
+
+    @Provides
+    fun activity() = activityCompositionRoot.activity
+
+    @Provides
+    fun screensNavigator() = activityCompositionRoot.screensNavigator
+
+    @Provides
+    fun viewMvcFactory(layoutInflater: LayoutInflater) = ViewMvcFactory(layoutInflater)
+
+    @Provides
+    fun dialogsNavigator(fragmentManager: FragmentManager) = DialogsNavigator(fragmentManager)
+
+    @Provides
+    fun fetchQuestionsUseCase(stackoverflowApi: StackoverflowApi) = FetchQuestionsUseCase(stackoverflowApi)
+
+    @Provides
+    fun fetchQuestionDetailsUseCase(stackoverflowApi: StackoverflowApi) = FetchQuestionDetailsUseCase(stackoverflowApi)
+
+}
