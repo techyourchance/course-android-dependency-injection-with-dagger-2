b115af1a6b2b0c8e8f530c5acfbfe48cb9a2de66
Vasiliy Zukanov <techyourchance@gmail.com>
2020-07-14 13:51:33 +0300

    Making ActivityComponent depend on AppComponent through Dagger convention

--- app/src/main/java/com/techyourchance/dagger2course/common/dependnecyinjection/activity/ActivityComponent.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/dependnecyinjection/activity/ActivityComponent.kt
@@ -3,12 +3,13 @@ package com.techyourchance.dagger2course.common.dependnecyinjection.activity
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
+import com.techyourchance.dagger2course.common.dependnecyinjection.app.AppComponent
 import com.techyourchance.dagger2course.networking.StackoverflowApi
 import com.techyourchance.dagger2course.screens.common.ScreensNavigator
 import dagger.Component
 
 @ActivityScope
-@Component(modules = [ActivityModule::class])
+@Component(dependencies = [AppComponent::class], modules = [ActivityModule::class])
 interface ActivityComponent {
 
     fun activity(): AppCompatActivity
