1dc08dae79603eecc884b3321bbf214f2e3de514
Vasiliy Zukanov <techyourchance@gmail.com>
2020-07-14 17:23:45 +0300

    Refactoring ActivityComponent to become subcomponent of AppComponent

--- app/src/main/java/com/techyourchance/dagger2course/common/dependnecyinjection/activity/ActivityComponent.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/dependnecyinjection/activity/ActivityComponent.kt
@@ -1,17 +1,11 @@
 package com.techyourchance.dagger2course.common.dependnecyinjection.activity
 
-import android.view.LayoutInflater
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
-import com.techyourchance.dagger2course.common.dependnecyinjection.app.AppComponent
 import com.techyourchance.dagger2course.common.dependnecyinjection.presentation.PresentationComponent
 import com.techyourchance.dagger2course.common.dependnecyinjection.presentation.PresentationModule
-import com.techyourchance.dagger2course.networking.StackoverflowApi
-import com.techyourchance.dagger2course.screens.common.ScreensNavigator
-import dagger.Component
+import dagger.Subcomponent
 
 @ActivityScope
-@Component(dependencies = [AppComponent::class], modules = [ActivityModule::class])
+@Subcomponent(modules = [ActivityModule::class])
 interface ActivityComponent {
 
     fun newPresentationComponent(presentationModule: PresentationModule): PresentationComponent
--- app/src/main/java/com/techyourchance/dagger2course/common/dependnecyinjection/presentation/PresentationComponent.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/dependnecyinjection/presentation/PresentationComponent.kt
@@ -1,9 +1,7 @@
 package com.techyourchance.dagger2course.common.dependnecyinjection.presentation
 
-import com.techyourchance.dagger2course.common.dependnecyinjection.activity.ActivityComponent
 import com.techyourchance.dagger2course.screens.questiondetails.QuestionDetailsActivity
 import com.techyourchance.dagger2course.screens.questionslist.QuestionsListFragment
-import dagger.Component
 import dagger.Subcomponent
 
 @PresentationScope
