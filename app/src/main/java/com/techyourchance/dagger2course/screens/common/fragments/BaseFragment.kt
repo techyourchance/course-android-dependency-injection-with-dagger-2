d470bfb91f32e74b46e4f67db96d73e1bc455947
Vasiliy Zukanov <techyourchance@gmail.com>
2020-07-14 17:11:52 +0300

    Refactoring PresentationCompoenent to become subcomponent of ActivityComponent

--- app/src/main/java/com/techyourchance/dagger2course/screens/common/fragments/BaseFragment.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/common/fragments/BaseFragment.kt
@@ -1,17 +1,13 @@
 package com.techyourchance.dagger2course.screens.common.fragments
 
 import androidx.fragment.app.Fragment
-import com.techyourchance.dagger2course.common.dependnecyinjection.presentation.DaggerPresentationComponent
 import com.techyourchance.dagger2course.common.dependnecyinjection.presentation.PresentationModule
 import com.techyourchance.dagger2course.screens.common.activities.BaseActivity
 
 open class BaseFragment: Fragment() {
 
     private val presentationComponent by lazy {
-        DaggerPresentationComponent.builder()
-                .activityComponent((requireActivity() as BaseActivity).activityComponent)
-                .presentationModule(PresentationModule())
-                .build()
+        (requireActivity() as BaseActivity).activityComponent.newPresentationComponent(PresentationModule())
     }
 
     protected val injector get() = presentationComponent
