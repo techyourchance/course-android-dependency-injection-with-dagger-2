518d1f2119aa9e0dcbbbc5ee59b3b8b4a6cc05ce
Vasiliy Zukanov <techyourchance@gmail.com>
2020-07-15 17:03:30 +0300

    Revert to providing ScreensNavigator from ActivityModule to manage scoping in one place

--- app/src/main/java/com/techyourchance/dagger2course/common/dependnecyinjection/activity/ActivityModule.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/dependnecyinjection/activity/ActivityModule.kt
@@ -2,6 +2,7 @@ package com.techyourchance.dagger2course.common.dependnecyinjection.activity
 
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import com.techyourchance.dagger2course.screens.common.ScreensNavigator
 import dagger.Module
 import dagger.Provides
 
@@ -13,6 +14,9 @@ class ActivityModule(
     @Provides
     fun activity() = activity
 
+    @Provides
+    @ActivityScope
+    fun screensNavigator(activity: AppCompatActivity) = ScreensNavigator(activity)
 
     @Provides
     fun layoutInflater(activity: AppCompatActivity) = LayoutInflater.from(activity)
