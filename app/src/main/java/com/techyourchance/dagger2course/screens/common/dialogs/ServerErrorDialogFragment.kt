23b1ec016d60e1c88e9b369b9ccac87d5ca81a0b
Vasiliy Zukanov <techyourchance@gmail.com>
2020-07-18 11:34:52 +0300

    Introduction of BaseDialog and DI infrastructure for dialogs

--- app/src/main/java/com/techyourchance/dagger2course/screens/common/dialogs/ServerErrorDialogFragment.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/common/dialogs/ServerErrorDialogFragment.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import com.techyourchance.dagger2course.R
 
-class ServerErrorDialogFragment : DialogFragment() {
+class ServerErrorDialogFragment : BaseDialog() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return AlertDialog.Builder(activity).let {
