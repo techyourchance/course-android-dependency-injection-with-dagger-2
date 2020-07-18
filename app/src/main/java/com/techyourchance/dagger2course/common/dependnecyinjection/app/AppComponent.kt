2090cea5dac6770004438496d73ce5a5f8cec814
Vasiliy Zukanov <techyourchance@gmail.com>
2020-07-18 11:26:54 +0300

    Introduction of BaseService and DI infrastructure for services

--- app/src/main/java/com/techyourchance/dagger2course/common/dependnecyinjection/app/AppComponent.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/dependnecyinjection/app/AppComponent.kt
@@ -2,6 +2,8 @@ package com.techyourchance.dagger2course.common.dependnecyinjection.app
 
 import com.techyourchance.dagger2course.common.dependnecyinjection.activity.ActivityComponent
 import com.techyourchance.dagger2course.common.dependnecyinjection.activity.ActivityModule
+import com.techyourchance.dagger2course.common.dependnecyinjection.service.ServiceComponent
+import com.techyourchance.dagger2course.common.dependnecyinjection.service.ServiceModule
 import dagger.Component
 
 @AppScope
@@ -9,5 +11,7 @@ import dagger.Component
 interface AppComponent {
 
     fun newActivityComponent(activityModule: ActivityModule): ActivityComponent
+
+    fun newServiceComponent(serviceModule: ServiceModule): ServiceComponent
 
 }
