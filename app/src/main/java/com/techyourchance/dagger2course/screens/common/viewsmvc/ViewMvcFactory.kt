43ca1003a95b2ed6ef98aa24bfc01346f5ba3327
Vasiliy Zukanov <techyourchance@gmail.com>
2020-07-19 18:09:42 +0300

    Using Provider's to avoid 'accidental scoping' of services by ViewMvcFactory

--- app/src/main/java/com/techyourchance/dagger2course/screens/common/viewsmvc/ViewMvcFactory.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/common/viewsmvc/ViewMvcFactory.kt
@@ -6,17 +6,18 @@ import com.techyourchance.dagger2course.screens.common.imageloader.ImageLoader
 import com.techyourchance.dagger2course.screens.questiondetails.QuestionDetailsViewMvc
 import com.techyourchance.dagger2course.screens.questionslist.QuestionsListViewMvc
 import javax.inject.Inject
+import javax.inject.Provider
 
 class ViewMvcFactory @Inject constructor(
-        private val layoutInflater: LayoutInflater,
-        private val imageLoader: ImageLoader
+        private val layoutInflaterProvider: Provider<LayoutInflater>,
+        private val imageLoaderProvider: Provider<ImageLoader>
 ) {
 
     fun newQuestionsListViewMvc(parent: ViewGroup?): QuestionsListViewMvc {
-        return QuestionsListViewMvc(layoutInflater, parent)
+        return QuestionsListViewMvc(layoutInflaterProvider.get(), parent)
     }
 
     fun newQuestionDetailsViewMvc(parent: ViewGroup?): QuestionDetailsViewMvc {
-        return QuestionDetailsViewMvc(layoutInflater, imageLoader, parent)
+        return QuestionDetailsViewMvc(layoutInflaterProvider.get(), imageLoaderProvider.get(), parent)
     }
 }
