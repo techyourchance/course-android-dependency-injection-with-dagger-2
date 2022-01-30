f0fcd73cab2b9e2fc4cb9faee2fa5510c9c1a37a
vibhanshu sharma <vibhanshusharma@vibhanshus-MBP.domain.name>
2022-01-30 23:47:39 +0530

    "adding ui mvc"

--- app/src/main/java/com/techyourchance/dagger2course/screens/questionslist/QuestionsListActivity.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/questionslist/QuestionsListActivity.kt
@@ -2,65 +2,47 @@ package com.techyourchance.dagger2course.screens.questionslist
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.techyourchance.dagger2course.Constants
-import com.techyourchance.dagger2course.R
 import com.techyourchance.dagger2course.networking.StackoverflowApi
 import com.techyourchance.dagger2course.questions.Question
 import com.techyourchance.dagger2course.screens.common.dialogs.ServerErrorDialogFragment
 import com.techyourchance.dagger2course.screens.questiondetails.QuestionDetailsActivity
-import com.techyourchance.dagger2course.screens.questionslist.QuestionsListActivity.QuestionsAdapter.QuestionViewHolder
 import kotlinx.coroutines.*
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.*
 
-class QuestionsListActivity : AppCompatActivity() {
+class QuestionsListActivity : AppCompatActivity(), QuestionsListViewMvc.Listener {
 
+
+    //activity doesnot know anything about implementation deatils or logic, it only tells controller class
+    // to perform the actions it doesnot care about the implemtation details.
     private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
 
-    private lateinit var swipeRefresh: SwipeRefreshLayout
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var questionsAdapter: QuestionsAdapter
+
     private lateinit var stackoverflowApi: StackoverflowApi
 
     private var isDataLoaded = false
 
+    private lateinit var viewMvc: QuestionsListViewMvc
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_questions_list)
+        setContentView(viewMvc.rootView)
 
-        // init pull-down-to-refresh
-        swipeRefresh = findViewById(R.id.swipeRefresh)
-        swipeRefresh.setOnRefreshListener {
-            fetchQuestions()
-        }
-
-        // init recycler view
-        recyclerView = findViewById(R.id.recycler)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        questionsAdapter = QuestionsAdapter{ clickedQuestion ->
-            QuestionDetailsActivity.start(this, clickedQuestion.id)
-        }
-        recyclerView.adapter = questionsAdapter
+        viewMvc = QuestionsListViewMvc(LayoutInflater.from(this), null)
 
         // init retrofit
         val retrofit = Retrofit.Builder()
-                .baseUrl(Constants.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
         stackoverflowApi = retrofit.create(StackoverflowApi::class.java)
     }
 
     override fun onStart() {
         super.onStart()
+        viewMvc.registerListener(this)
         if (!isDataLoaded) {
             fetchQuestions()
         }
@@ -68,16 +50,17 @@ class QuestionsListActivity : AppCompatActivity() {
 
     override fun onStop() {
         super.onStop()
+        viewMvc.unregisterListener(this)
         coroutineScope.coroutineContext.cancelChildren()
     }
 
     private fun fetchQuestions() {
         coroutineScope.launch {
-            showProgressIndication()
+            viewMvc.showProgressIndication()
             try {
                 val response = stackoverflowApi.lastActiveQuestions(20)
                 if (response.isSuccessful && response.body() != null) {
-                    questionsAdapter.bindData(response.body()!!.questions)
+                    viewMvc.bindQuestions(response.body()!!.questions)
                     isDataLoaded = true
                 } else {
                     onFetchFailed()
@@ -87,58 +70,25 @@ class QuestionsListActivity : AppCompatActivity() {
                     onFetchFailed()
                 }
             } finally {
-                hideProgressIndication()
+                viewMvc.hideProgressIndication()
             }
         }
     }
 
     private fun onFetchFailed() {
         supportFragmentManager.beginTransaction()
-                .add(ServerErrorDialogFragment.newInstance(), null)
-                .commitAllowingStateLoss()
+            .add(ServerErrorDialogFragment.newInstance(), null)
+            .commitAllowingStateLoss()
     }
 
-    private fun showProgressIndication() {
-        swipeRefresh.isRefreshing = true
+
+    override fun onRefreshClicked() {
+        fetchQuestions()
     }
 
-    private fun hideProgressIndication() {
-        if (swipeRefresh.isRefreshing) {
-            swipeRefresh.isRefreshing = false
-        }
+    override fun onQuestionClicked(clickedQuestion: Question) {
+        QuestionDetailsActivity.start(this, clickedQuestion.id)
     }
 
-    class QuestionsAdapter(
-            private val onQuestionClickListener: (Question) -> Unit
-    ) : RecyclerView.Adapter<QuestionViewHolder>() {
 
-        private var questionsList: List<Question> = ArrayList(0)
-
-        inner class QuestionViewHolder(view: View) : ViewHolder(view) {
-            val title: TextView = view.findViewById(R.id.txt_title)
-        }
-
-        fun bindData(questions: List<Question>) {
-            questionsList = ArrayList(questions)
-            notifyDataSetChanged()
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuestionViewHolder {
-            val itemView = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.layout_question_list_item, parent, false)
-            return QuestionViewHolder(itemView)
-        }
-
-        override fun onBindViewHolder(holder: QuestionViewHolder, position: Int) {
-            holder.title.text = questionsList[position].title
-            holder.itemView.setOnClickListener {
-                onQuestionClickListener.invoke(questionsList[position])
-            }
-        }
-
-        override fun getItemCount(): Int {
-            return questionsList.size
-        }
-
-    }
 }
---
+++ app/src/main/java/com/techyourchance/dagger2course/screens/questionslist/QuestionsListViewMvc.kt
@@ -0,0 +1,130 @@
+package com.techyourchance.dagger2course.screens.questionslist
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.IdRes
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.techyourchance.dagger2course.R
+import com.techyourchance.dagger2course.questions.Question
+import java.util.ArrayList
+
+
+// it contains all the user interface logic,
+class QuestionsListViewMvc(
+    layoutInflater: LayoutInflater,
+    parent: ViewGroup?
+) {
+
+
+    interface Listener {
+        fun onRefreshClicked()
+        fun onQuestionClicked(clickedQuestion: Question)
+    }
+
+    private var swipeRefresh: SwipeRefreshLayout
+    private var recyclerView: RecyclerView
+    private val questionsAdapter: QuestionsAdapter
+
+    val rootView: View =
+        layoutInflater.inflate(
+            R.layout.layout_questions_list,
+            parent,
+            false
+        )
+    private val context: Context get() = rootView.context
+
+
+    //yaha pe multiple views isliye bana diye hai taaki ek common controller ho ske
+    // multiple views ke liye so that so that if we want to notify multiple views we can do that.
+    val listeners = HashSet<Listener>()
+
+
+    init {
+
+        // init pull-down-to-refresh
+        swipeRefresh = findViewById(R.id.swipeRefresh)
+        swipeRefresh.setOnRefreshListener {
+            for (listener in listeners) {
+                listener.onRefreshClicked()
+            }
+        }
+
+        // init recycler view
+        recyclerView = findViewById(R.id.recycler)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        questionsAdapter = QuestionsAdapter { clickedQuestion ->
+            for (listener in listeners) {
+                listener.onQuestionClicked(clickedQuestion)
+            }
+        }
+        recyclerView.adapter = questionsAdapter
+    }
+
+    private fun <T : View?> findViewById(@IdRes id: Int): T {
+        return rootView.findViewById<T>(id)
+    }
+
+    fun registerListener(listener: Listener) {
+        listeners.add(listener)
+    }
+
+    fun unregisterListener(listener: Listener) {
+        listeners.remove(listener)
+    }
+
+    fun showProgressIndication() {
+        swipeRefresh.isRefreshing = true
+    }
+
+    fun hideProgressIndication() {
+        if (swipeRefresh.isRefreshing) {
+            swipeRefresh.isRefreshing = false
+        }
+    }
+
+    fun bindQuestions(questions: List<Question>) {
+        questionsAdapter.bindData(questions)
+    }
+
+
+    class QuestionsAdapter(
+        private val onQuestionClickListener: (Question) -> Unit
+    ) : RecyclerView.Adapter<QuestionsAdapter.QuestionViewHolder>() {
+
+        private var questionsList: List<Question> = ArrayList(0)
+
+        inner class QuestionViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val title: TextView = view.findViewById(R.id.txt_title)
+        }
+
+        fun bindData(questions: List<Question>) {
+            questionsList = ArrayList(questions)
+            notifyDataSetChanged()
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuestionViewHolder {
+            val itemView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.layout_question_list_item, parent, false)
+            return QuestionViewHolder(itemView)
+        }
+
+        override fun onBindViewHolder(holder: QuestionViewHolder, position: Int) {
+            holder.title.text = questionsList[position].title
+            holder.itemView.setOnClickListener {
+                onQuestionClickListener.invoke(questionsList[position])
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return questionsList.size
+        }
+
+    }
+
+
+}
