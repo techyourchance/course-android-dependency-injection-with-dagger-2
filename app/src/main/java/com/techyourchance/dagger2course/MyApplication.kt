74849fad552204e8c0057a7a2d717579ee6511cd
rohitsurthi <imrohitsurthi@gmail.com>
2023-11-04 14:33:37 +0530

    - checking

--- app/src/main/java/com/techyourchance/dagger2course/MyApplication.kt
+++ app/src/main/java/com/techyourchance/dagger2course/MyApplication.kt
@@ -3,9 +3,7 @@ package com.techyourchance.dagger2course
 import android.app.Application
 
 class MyApplication: Application() {
-
     override fun onCreate() {
         super.onCreate()
     }
-
 }
